d31121dacb5e857bbacdd2397eff7766a860d8ae
Gregory Bouteiller <gregory.bouteiller@niama.re>
2019-03-01 08:22:38 +0400

    Update app.authenticated.handler.ts

--- project/app/components/app.authenticated.handler.ts
+++ project/app/components/app.authenticated.handler.ts
@@ -3,13 +3,6 @@ import { AuthService } from '~/auth/services/auth.service';
 
 @Component({
   template: `
-    <ActionBar title="" class="action-bar">
-      <Image horizontalAlignment="left" src="~/images/logo-blanc.png" height="40"></Image>
-      <ActionItem (tap)="onTapSignout()" class="action-item">
-        <Button text="\uf011" width="60" borderWidth="1" borderColor="transparent" class="fas btn btn-accent" ></Button>
-      </ActionItem>
-    </ActionBar>
-    <router-outlet></router-outlet>
   `,
 })
 export class AppAuthenticatedHandler {
